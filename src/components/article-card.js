8562327b3a52bd38356351af3d849dffa9411abd
tsengjonathan <github@tsengjonathan.com>
2020-07-29 21:42:04 -0400

    Abstract articles to own component on index page

---
+++ src/components/article-card.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { Link } from "gatsby"
+import { rhythm } from "../utils/typography"
+
+function ArticleCard({node}) {
+  const title = node.frontmatter.title || node.fields.slug
+  return (
+    <article key={node.fields.slug}>
+      <header>
+        <h3
+          style={{
+            marginBottom: rhythm(1 / 4),
+          }}
+        >
+          <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+            {title}
+          </Link>
+        </h3>
+        <small>{node.frontmatter.date}</small>
+      </header>
+      <section>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: node.excerpt,
+          }}
+        />
+      </section>
+    </article>
+  )
+}
+
+export default ArticleCard
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import ArticleCard from "../components/article-card"
 
 const ArticleIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -14,32 +14,7 @@ const ArticleIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
+      {posts.map(({ node }) => <ArticleCard node={node}/>)}
     </Layout>
   )
 }
