429f5df0954fbb6ff5004d5eca11a6d986f1d7f1
Jonathan Tseng <7547794+tsengjonathan@users.noreply.github.com>
2020-08-19 23:04:33 -0400

    Add Article Bootstrap GitHub Action (#1)
    * Wrap ref in string quotations
    
    * Include SHA when creating branch
    
    * Initial attempt at creating sample file
    
    * Get branch SHA
    
    * Wrap branch name in quotations
    
    * Fix extra quotation
    
    * Fix typo in input variable lookup
    
    * Remove quotations from input lookup
    
    * Add dependency on branch job
    
    * Remove extra quotation
    
    * Initial attempt at creating commit
    
    * Wrap parents in array
    
    * Debug tree structure
    
    * Debug head
    
    * Debug head commit tree
    
    * Attempt at creating tree
    
    * Use blob SHA for tree
    
    * Initial attempt at creating commit
    
    * Add base_tree when creating tree
    
    * Update head reference
    
    * Add missing comma
    
    * Use proper ref name
    
    * Get ref
    
    * Fix typo on repo
    
    * Add dollar sign to ref lookup
    
    * Update ref
    
    * Refactor and move script to separate file
    
    * Indent script
    
    * Add actions/checkout as a dependency
    
    * Properly use JS dates
    
    * Use proper block string notation
    
    * Debug context input
    
    * Await module function
    
    * Wrap input in object
    
    * Fix input lookup
    
    * Debug github parameter
    
    * Test workflow dispatch interpolation
    
    * Debug endpoint input
    
    * Pass endpoint as function argument
    
    * Add actor as commit author
    
    * Add null email to author
    
    * Remove custom authoring commits
    
    Co-authored-by: Jonathan Tseng <tsengjonathan@users.noreply.github.com>

---
+++ src/actions/article.js
@@ -0,0 +1,52 @@
+const currentDate = new Date();
+
+const articleContent = 
+`---
+title: 
+date: '${currentDate.toISOString()}'
+cover: 
+---
+`
+
+module.exports = async ({ github, context, endpoint }) => {
+  const defaultArgs = {
+    owner: context.repo.owner,
+    repo: context.repo.repo
+  }
+
+  console.log("Endpoint", endpoint)
+
+  const { data: { commit: branch } } = await github.repos.getBranch({
+    ...defaultArgs,
+    branch: endpoint
+  })
+
+  const { data: blob } = await github.git.createBlob({
+    ...defaultArgs,
+    content: articleContent
+  })
+
+  const { data: tree } = await github.git.createTree({
+    ...defaultArgs,
+    tree: [{
+      path: `content/articles/${endpoint}/index.md`,
+      mode: "100644",
+      type: "blob",
+      sha: blob.sha
+    }],
+    base_tree: branch.commit.tree.sha
+  })
+
+  const { data: commit } = await github.git.createCommit({
+    ...defaultArgs,
+    message: "Initialize file",
+    tree: tree.sha,
+    parents: [branch.sha]
+  })
+
+  await github.git.updateRef({
+    ...defaultArgs,
+    ref: `heads/${endpoint}`,
+    sha: commit.sha,
+  })
+}
