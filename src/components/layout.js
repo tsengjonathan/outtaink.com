6d31d4ce79376e84d19c1b77185ec10e412a7dc2
tsengjonathan <github@tsengjonathan.com>
2020-12-02 18:25:21 -0500

    Remove footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,13 +6,6 @@ const Layout = ({ location, title, children }) => {
     <div className="px-8 py-4 flex flex-col min-h-screen">
       <Header title={title} />
       <main className="flex flex-1">{children}</main>
-      <footer>
-        <p>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </p>
-      </footer>
     </div>
   );
 };
