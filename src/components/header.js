fbedd8ecfeb947b5711ef871ea61214a6d8b31f8
tsengjonathan <github@tsengjonathan.com>
2020-12-05 19:57:32 -0500

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-export default function Header({title}) {
+export default function Header() {
   return (
-    <h3 className="my-0">
-      <Link
-        className="shadow-none text-current no-underline"
-        to={`/`}
-      >
-        {title}
-      </Link>
-    </h3>
-  )
+    <div className="h-screen w-20 border-0 border-r border-solid border-black">
+      <h1 className="writing-mode-vertical text-orientation-sideways absolute transform translate-x-6 mt-8">
+        <Link className="shadow-none text-current no-underline" to={`/`}>
+          Outtaink: 記錄留學生的故事
+        </Link>
+      </h1>
+    </div>
+  );
 }
