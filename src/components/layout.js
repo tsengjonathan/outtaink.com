fbc7a05966ce898d19cbe2c2a5c1732f7a5de19b
tsengjonathan <github@tsengjonathan.com>
2020-12-05 19:59:50 -0500

    Update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from '../components/header';
 
-const Layout = ({ location, title, children }) => {
+const Layout = ({ children }) => {
   return (
-    <div className="px-8 py-4 flex flex-col min-h-screen">
-      <Header title={title} />
-      <main className="flex flex-1">{children}</main>
+    <div className="flex">
+      <Header />
+      <main className="flex flex-1 h-screen">{children}</main>
     </div>
   );
 };
