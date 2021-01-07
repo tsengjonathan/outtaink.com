492f6be027ed3e74e2a18adfcf416a8db2668699
tsengjonathan <github@tsengjonathan.com>
2021-01-06 22:04:16 -0500

    Add logic to submit email using Netlify's Form

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
 
 export default function Newsletter() {
-    const borderStyle = 'border-0 border-black border-solid border-b'
-    return (
-        <div className={`h-32 flex ${borderStyle}`}>
-            <div className='flex flex-1 flex-col justify-center items-center text-sm'>
-                <div>加入我們的 Newsletter 一起發現更多最新故事吧！</div>
-                <div className={`w-1/3 flex ${borderStyle}`}>
-                    <input type="text" name="price" id="price" className="flex-1 pl-2 pr-12 border-0 focus:outline-none" placeholder="Your email" />
-                    <button type="button" className="border-0 bg-white cursor-pointer">JOIN</button>
-                </div>
-            </div>
+  const borderStyle = 'border-0 border-black border-solid border-b';
+
+  const [state, setState] = useState({ email: '' });
+  const { email } = state;
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    }).catch(error => console.error(error));
+  };
+
+  return (
+    <div className={`h-32 flex ${borderStyle}`}>
+      <div className="flex flex-1 flex-col justify-center items-center text-sm">
+        <div>加入我們的 Newsletter 一起發現更多最新故事吧！</div>
+        <div className={`w-1/3 flex`}>
+          <form
+            name="email"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            className={`flex flex-1 m-0 ${borderStyle}`}
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="email"
+              name="email"
+              className="flex-1 py-2 border-0 focus:outline-none"
+              placeholder="Your email"
+              value={email}
+              onChange={handleChange}
+            />
+            <button type="submit" className="border-0 bg-white cursor-pointer">
+              JOIN
+            </button>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
