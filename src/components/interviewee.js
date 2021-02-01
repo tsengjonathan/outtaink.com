95de461b7b21e7fee5d9c7ad9539d8c33b885518
tsengjonathan <github@tsengjonathan.com>
2021-01-31 21:07:46 -0500

    Tweak font weighting

--- src/components/interviewee.js
+++ src/components/interviewee.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Interviewee({ name, bio, links }) {
   return (
-    <div className="h-screen sticky top-12 mt-32 ml-12 mb-12">
-      <p className="font-sans mb-4">{name}</p>
-      <p className="font-sans mb-4 font-extralight text-sm leading-5 text-gray-600">{bio}</p>
+    <div className="h-screen sticky lg:top-12 lg:mt-32 lg:ml-12 lg:mb-12">
+      <p className="font-sans mb-3 font-medium">{name}</p>
+      <p className="font-sans mb-3 font-light text-left leading-5 text-gray-600">{bio}</p>
       <section
-        className="font-sans font-extralight text-sm leading-6"
+        className="font-sans font-light leading-6"
         dangerouslySetInnerHTML={{ __html: links }}
       />
     </div>
