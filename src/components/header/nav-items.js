7bcaaa0846d201189a69213ca0ffc1aaebf9a2c2
tsengjonathan <github@tsengjonathan.com>
2021-02-20 11:57:11 -0500

    Fix nav item hover transition

--- src/components/header/nav-items.js
+++ src/components/header/nav-items.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 const webBorder = 'border-0 border-gray-500 border-solid border-l';
 const mobileBorder = 'border-0 border-solid border-b';
 const commonStyle =
-  'flex-1 flex-col justify-center items-center font-sans font-medium no-underline';
+  'nav-item flex-1 flex-col justify-center items-center font-sans font-medium no-underline';
 
 export default function NavItems({ navItems, isMobile }) {
   return navItems.map((nav, idx) => (
