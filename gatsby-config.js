8babcd0e73bc2d1012a2f85d30c2ad225b2070c2
Jonathan Tseng <github@tsengjonathan.com>
2021-02-20 14:34:49 -0500

    Miscellaneous fixes (#28)
    * Update site metadata
    
    * Remove Segment plugin
    
    * Fix SEO title on home page
    
    * Change SEO title for index page to Featured
    
    * Cleanup social tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,16 +6,12 @@ var linkResolver = require('./linkResolver');
 
 module.exports = {
   siteMetadata: {
-    title: `OuttaInk`,
+    title: `Outtaink`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jonathan Tseng`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `A community for students to voice their experiences and lessons as they embrace the challenges of studying abroad.`,
+    siteUrl: `https://outtaink.com/`,
   },
   pathPrefix: '/outtaink.com',
   plugins: [
@@ -34,45 +30,6 @@ module.exports = {
           article: require('./src/schemas/article.json'),
           author: require('./src/schemas/author.json'),
         },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-segment-js`,
-      options: {
-        // your segment write key for your production environment
-        // when process.env.NODE_ENV === 'production'
-        // required; non-empty string
-        prodKey: `r1BQEKQCHHZrEbPniJQhf6Q1B1Cc0QuF`,
-
-        // if you have a development env for your segment account, paste that key here
-        // when process.env.NODE_ENV === 'development'
-        // optional; non-empty string
-        devKey: `lPHDbhUfQSEgXF2Kje4AC91ECM2n8lgR`,
-
-        // boolean (defaults to false) on whether you want
-        // to include analytics.page() automatically
-        // if false, see below on how to track pageviews manually
-        trackPage: true,
-
-        // boolean (defaults to false); whether to delay load Segment
-        // ADVANCED FEATURE: only use if you leverage client-side routing (ie, Gatsby <Link>)
-        // This feature will force Segment to load _after_ either a page routing change
-        // or user scroll, whichever comes first. This delay time is controlled by
-        // `delayLoadTime` setting. This feature is used to help improve your website's
-        // TTI (for SEO, UX, etc).  See links below for more info.
-        // NOTE: But if you are using server-side routing and enable this feature,
-        // Segment will never load (because although client-side routing does not do
-        // a full page refresh, server-side routing does, thereby preventing Segment
-        // from ever loading).
-        // See here for more context:
-        // GIF: https://github.com/benjaminhoffman/gatsby-plugin-segment-js/pull/19#issuecomment-559569483
-        // TTI: https://github.com/GoogleChrome/lighthouse/blob/master/docs/scoring.md#performance
-        // Problem/solution: https://marketingexamples.com/seo/performance
-        delayLoad: true,
-
-        // number (default to 1000); time to wait after scroll or route change
-        // To be used when `delayLoad` is set to `true`
-        delayLoadTime: 1000,
       },
     },
     `gatsby-plugin-sitemap`,
