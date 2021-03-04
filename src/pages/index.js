37f952d25dd950a2769003f056888a3ec8928203
tsengjonathan <github@tsengjonathan.com>
2021-03-04 14:20:11 -0500

    Mark first 3 articles as featured

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import ArticleCard from '../components/article-card';
 import Newsletter from '../components/newsletter';
+import Featured from '../components/featured';
 
 import '../styles/index.css';
 
@@ -12,13 +13,17 @@ const ArticleIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title;
   const posts = data.allPrismicArticle.edges;
 
-  const midPoint = Math.floor(posts.length / 2);
-  const leftArticles = posts.slice(0, midPoint);
-  const rightArticles = posts.slice(midPoint, posts.length);
+  const featuredArticles = posts.slice(0, 3);
+  const remainingArticles = posts.slice(3);
+
+  const midPoint = Math.floor(remainingArticles.length / 2);
+  const leftArticles = remainingArticles.slice(0, midPoint);
+  const rightArticles = remainingArticles.slice(midPoint, posts.length);
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Featured" />
+      <Featured articles={featuredArticles} />
       <Newsletter />
       <div className="my-8 mx-4 lg:mx-10 flex flex-col lg:flex-row">
         <div className="h-full lg:w-7/12 lg:mr-10">
