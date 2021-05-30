1953e75e57c3bc3a792ec2c49c9dbde39ef59ee6
Jonathan Tseng <me@tsengjonathan.com>
2021-05-29 22:58:45 -0400

    Update selected styles on button

--- src/components/filter/button.js
+++ src/components/filter/button.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function FilterButton({ name, selected, onClick }) {
     const border = 'border border-solid border-black rounded-full'
 
-    const selectedStyles = selected ? 'bg-default-50' : ''
+    const selectedStyles = selected ? 'bg-default-50 text-white' : ''
 
     return (
-        <div onClick={onClick} className={`mx-2 py-1 px-4 text-sm cursor-pointer ${selectedStyles} ${border}`}>
+        <div onClick={onClick} className={`mx-2 py-1 px-4 text-sm font-zh cursor-pointer ${selectedStyles} ${border}`}>
             {name}
         </div>
     )
