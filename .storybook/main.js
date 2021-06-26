a4863d6727d145e3c8bd50cdab93442f81003f82
Jonathan Tseng <me@tsengjonathan.com>
2021-06-26 16:25:04 -0400

    Fix issue with Gatsby Links in Storybook components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,23 @@
 module.exports = {
-  "core": {
+  core: {
     "builder": "webpack5"
   },
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async config => {
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    // use babel-plugin-remove-graphql-queries 
+    // to remove static queries from components when rendering in storybook
+    config.module.rules[0].use[0].options.plugins.push(
+        require.resolve("babel-plugin-remove-graphql-queries")
+    )
+    return config
+  },
 }
