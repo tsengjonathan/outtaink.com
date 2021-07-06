2cd119bfd90eb7cee66e7f3899418fa09612d5f6
Jonathan Tseng <me@tsengjonathan.com>
2021-07-05 21:01:57 -0400

    Update navigation bar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'gatsby';
 
 import NavItems from './nav-items';
+import Logo from '../Logo';
 import MobileLogo from '../../../content/svg/mobile-logo.svg';
 import MobileNavOpen from '../../../content/svg/mobile-nav-open.svg';
 
@@ -11,18 +12,22 @@ const navs = [
   { name: '關於', subname: 'About', src: '/' },
 ];
 
+const bottomBorder = 'border-b border-gray-500 border-solid';
+
 export default function Header() {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <div className="h-14 flex my-auto lg:flex-row items-start lg:items-center border-0 border-gray-500 border-solid border-b">
+    <div className={`flex my-auto lg:flex-row items-start lg:items-center lg:justify-between lg:mx-4 ${bottomBorder} lg:border-0`}>
       <button
         className="border-0 bg-transparent w-14 h-full items-center justify-center mx-4 p-2.5 cursor-pointer lg:hidden"
         onClick={() => setNavOpen(!navOpen)}
       >
         <MobileNavOpen />
       </button>
+      <Logo className="hidden lg:block" color="#000" />
       <NavItems navItems={navs} isMobile={false} />
+      <div className="w-8" />
       <Link
         className="flex-grow w-20 lg:hidden h-full border-0 border-solid border-l no-underline"
         to="/"
--- src/components/header/nav-items.js
+++ src/components/header/nav-items.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const webBorder = 'border-0 border-gray-500 border-solid border-l';
 const mobileBorder = 'border-0 border-solid border-b';
-const commonStyle = 'nav-item flex-1 flex-col justify-center items-center font-sans font-medium no-underline';
+const commonStyle =
+  'nav-item flex-col justify-center items-center font-sans font-medium no-underline';
 
 const isBrowser = typeof window !== 'undefined';
 
 export default function NavItems({ navItems, isMobile }) {
-  const currentPage = isBrowser ? window.location.pathname : '/'
-  return navItems.map((nav, idx) => (
-    <Link
-      key={idx}
-      className={
-        isMobile
-          ? `${commonStyle} ${mobileBorder} flex min-h-20`
-          : `${commonStyle} ${
-              idx !== 0 ? webBorder : null
-            } h-full hidden lg:flex`
-      }
-      to={nav.src}
-    >
-      <p className={`font-medium font-zh ${nav.src === currentPage ? 'nav-item-selected' : 'highlight-text'}`}>{nav.name}</p>
-      <p
-        className={`text-default-50 font-bold font-en tracking-widest mt-0.5 ${
-          isMobile ? 'text-sm' : 'text-xs'
-        }`}
-      >
-        {nav.subname.toUpperCase()}
-      </p>
-    </Link>
-  ));
+  const currentPage = isBrowser ? window.location.pathname : '/';
+  return (
+    <div className="flex">
+      {navItems.map((nav, idx) => (
+        <Link
+          key={idx}
+          className={
+            isMobile
+              ? `${commonStyle} ${mobileBorder} flex flex-1 min-h-20`
+              : `${commonStyle} h-full hidden lg:flex m-4`
+          }
+          to={nav.src}
+        >
+          <p
+            className={`font-medium font-zh ${
+              nav.src === currentPage ? 'nav-item-selected' : 'highlight-text'
+            }`}
+          >
+            {nav.name}
+          </p>
+          <p
+            className={`text-default-50 font-bold font-en tracking-widest mt-0.5 ${
+              isMobile ? 'text-sm' : 'text-xs'
+            }`}
+          >
+            {nav.subname.toUpperCase()}
+          </p>
+        </Link>
+      ))}
+    </div>
+  );
 }
