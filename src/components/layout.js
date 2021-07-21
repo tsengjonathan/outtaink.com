41380f555c64eb28a670ff19629e7d6bb6be5d50
Jonathan Tseng <me@tsengjonathan.com>
2021-07-20 23:53:19 -0400

    Implement basic page transition

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Sidebar from './sidebar';
 import Header from './header';
 import Footer from './footer';
+import Transition from './Transition';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, location }) => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-sidebar">
       <Sidebar className="hidden lg:block" />
       <div className="flex flex-col">
         <Header />
-        <main className="flex flex-col flex-1">{children}</main>
+        <main className="flex flex-col flex-1">
+          <Transition location={location}>
+            {children}
+          </Transition>
+        </main>
         <Footer />
       </div>
     </div>
