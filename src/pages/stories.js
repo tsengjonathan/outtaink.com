f7bfd047b6bd0a01f4bd4892699ca5a694eb1eb3
Jonathan Tseng <me@tsengjonathan.com>
2021-07-28 23:00:23 -0400

    Add name to stories page query

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -59,6 +59,7 @@ export const pageQuery = graphql`
             title {
               text
             }
+            name
             author {
               document {
                 ... on PrismicAuthor {
