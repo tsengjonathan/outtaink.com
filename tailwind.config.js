f455a00f63172a449554a3f9138657ca02fcd92c
Jonathan Tseng <me@tsengjonathan.com>
2021-08-17 22:40:08 -0400

    Add debug script and fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
+  purge: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false,
   theme: {
     extend: {
