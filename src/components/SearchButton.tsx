bbc8aee66dfccb510c73eee2109aac49b993eb77
Jonathan Tseng <me@tsengjonathan.com>
2021-09-13 20:35:38 -0400

    Create components for SVG icons

---
+++ src/components/SearchButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchButton = () => {
+  const tooltipText = "Coming Soon!"
+
+  return (
+    <div data-tip={tooltipText} className="tooltip tooltip-left cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      </svg>
+    </div>
+  )
+}
+
+export default SearchButton
