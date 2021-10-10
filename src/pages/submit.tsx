73aca5b07fd3764f0d6eae5f03acf12a65e8e3df
Jonathan Tseng <me@tsengjonathan.com>
2021-10-10 16:30:16 -0400

    Optimize for mobile layouts

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -5,11 +5,13 @@ const SubmitPage = () => {
   const formId = 'GOaFCYbc'
 
   return (
-    <Widget
-      className="flex justify-center mx-4 my-6 md:mt-0 md:mb-12 filter drop-shadow-lg"
-      height={720} width={1080}
-      id={formId}
-    />
+    <div className="mx-4 my-6 md:mt-0 md:mb-12">
+      <Widget
+        className="flex justify-center filter drop-shadow-lg"
+        height={720} width={1080}
+        id={formId}
+      />
+    </div>
   )
 }
 
