d63ca284b7b4c64709444a703112c02f3d48e273
Jonathan Tseng <me@tsengjonathan.com>
2021-12-25 14:06:26 -0500

    Test Sentry Error on Vercel

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -11,7 +11,7 @@ const SearchButton: FC<SearchButtonProps> = ({ className }: SearchButtonProps) =
   const classes = classNames(className, 'tooltip cursor-pointer')
 
   return (
-    <div data-tip={tooltipText} className={classes}>
+    <div data-tip={tooltipText} className={classes} onClick={() => {throw Error('Sentry Test')}}>
       <svg xmlns="http://www.w3.org/2000/svg" className="h-7 md:h-9" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
       </svg>
