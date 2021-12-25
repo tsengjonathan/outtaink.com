6b023405338d5b8f957a93c8c0310c20cc94939e
Jonathan Tseng <me@tsengjonathan.com>
2021-12-25 15:23:46 -0500

    Update Sentry DSN

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: SENTRY_DSN || 'https://7fd621a40dc24c399e1905c6b94d9846@o1061541.ingest.sentry.io/6052223',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
