8866d7749b3c72770076d37c5ef1056c60d361bb
Jonathan Tseng <me@tsengjonathan.com>
2021-12-25 23:04:32 -0500

    Reimplement Drawer using MUI (#59)

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,39 +1,27 @@
-import React, { FC, MutableRefObject, ReactNode } from 'react'
+import React, { Dispatch, FC, SetStateAction } from 'react'
 
+import MUIDrawer from '@mui/material/Drawer'
 import { navs } from './Navigation'
 import NavigationItem from './NavigationItem'
 
 type DrawerProps = {
-  children: ReactNode[]
-  toggleId: string
-  scrollRef?: MutableRefObject<HTMLDivElement>
-  onScroll?: () => void
+  isOpen: boolean
+  setIsOpen: Dispatch<SetStateAction<boolean>>
 }
 
-const Drawer: FC<DrawerProps> = ({ children, toggleId, scrollRef, onScroll }: DrawerProps) => {
-  const liOnClick = () => {
-    const inputElements = document && document.getElementsByTagName('input')
-    const toggle = inputElements.namedItem(toggleId)
-    toggle.checked = false
-  }
+const Drawer: FC<DrawerProps> = ({ isOpen, setIsOpen }: DrawerProps) => {
+  const closeDrawer = () => setIsOpen(false)
 
   return (
-    <div className="drawer drawer-end">
-      <input id={toggleId} type="checkbox" className="drawer-toggle"/> 
-      <div className="drawer-content" ref={scrollRef} onScroll={onScroll}>
-        { children }
-      </div>
-      <div className="drawer-side">
-        <label className="drawer-overlay" htmlFor={toggleId} />
-        <ul className="menu w-80 bg-white py-6">
-          {navs.map(nav => (
-            <li onClick={liOnClick} key={nav.src}>
-              <NavigationItem to={nav.src} text={nav.name} hoverText={nav.subname} className="mx-auto my-4" />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <MUIDrawer open={isOpen} onClose={closeDrawer} anchor="right">
+      <ul className="menu w-80 bg-white py-6">
+        {navs.map(nav => (
+          <li onClick={closeDrawer} key={nav.src}>
+            <NavigationItem to={nav.src} text={nav.name} hoverText={nav.subname} className="mx-auto my-4" />
+          </li>
+        ))}
+      </ul>
+    </MUIDrawer>
   )
 }
 
