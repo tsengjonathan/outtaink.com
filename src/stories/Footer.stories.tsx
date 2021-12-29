5e23743a47176b096d515d7d267ff764e2b0704c
Jonathan Tseng <me@tsengjonathan.com>
2021-12-29 00:06:54 -0500

    Add Footer.stories.tsx

---
+++ src/stories/Footer.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ComponentStory } from '@storybook/react'
+import Footer from '../components/Footer'
+
+export default {
+  title: 'Footer',
+  component: Footer
+}
+
+const Template: ComponentStory<typeof Footer> = (args) => <Footer {...args} />
+
+export const Default = Template.bind({})
+Default.args = {}
