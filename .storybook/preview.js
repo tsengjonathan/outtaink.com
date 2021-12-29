c32984ff4fab2733c88c3f95b2b25dd59ffcd3fd
Jonathan Tseng <me@tsengjonathan.com>
2021-12-29 00:05:05 -0500

    Copy over CSS imports to .storybook/preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import '@fontsource/noto-sans-tc/300.css'
+import '@fontsource/noto-sans-tc/400.css'
+import '@fontsource/noto-sans-tc/500.css'
+import '@fontsource/noto-sans-tc/700.css'
+
+import 'prismjs/themes/prism.css'
+
+import '../src/styles/tailwind.css'
+import '../src/styles/global.css'
+import '../src/styles/index.css'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
